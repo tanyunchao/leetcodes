bd7ed85a71022cded4ddc8ab0375071b3368f579
yunchao <tanyunchao@gmail.com>
2023-10-21 23:31:16 +0800

    Contains Duplicate solution

---
+++ ContainsDuplicate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i : nums) {
+            if (!set.add(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
